4aba92e10b242004ca37df3fa28ae60f253dff31
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-22 22:12:48 -0300

    feat: add initial domain

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,21 @@
+import { randomUUID } from "node:crypto"
+
+interface QuestionProps {
+  title: string
+  content: string
+  authorId: string
+}
+
+export class Question {
+  public id: string
+  public title: string
+  public content: string
+  public authorId: string
+
+  constructor({ title, content, authorId }: QuestionProps, id?: string) {
+    this.title = title
+    this.content = content
+    this.authorId = authorId
+    this.id = id ?? randomUUID()
+  }
+}
