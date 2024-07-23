aa6249aea382ba48b9d297b31a97cbe76edaa858
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-23 17:27:53 -0300

    test: ensure CreateQuestionUseCase creates a question

---
+++ src/domain/forum/application/usecases/create-question.spec.ts
@@ -0,0 +1,18 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async () => {},
+}
+
+test('create a question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'Title',
+    content: 'New question',
+  })
+
+  expect(question.id).toBeTruthy()
+})
