c8ea14cbc0823013614ad03a0a2252cf0606cfc7
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-23 22:12:30 -0300

    refactor: use question factory

--- src/domain/forum/application/usecases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/usecases/get-question-by-slug.spec.ts
@@ -1,7 +1,6 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { Question } from '../../enterprise/entities/question'
-import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
@@ -14,10 +13,7 @@ describe('Get Question By Slug', () => {
   })
 
   it('should be able to find a Question by slug', async () => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityId(),
-      content: 'message',
-      title: 'My Question Title',
+    const newQuestion = makeQuestion({
       slug: Slug.create('my-question-title'),
     })
 
