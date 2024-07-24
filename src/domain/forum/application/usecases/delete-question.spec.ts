45ad20901655b04c4e57d23a794e32f2cc5c9e57
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-24 17:13:46 -0300

    test: ensure DeleteQuestionUseCase deletes question

---
+++ src/domain/forum/application/usecases/delete-question.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { DeleteQuestionUseCase } from './delete-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: DeleteQuestionUseCase
+
+describe('Delete Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to delete a question', async () => {
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityId('author-01') },
+      new UniqueEntityId('question-01'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      authorId: newQuestion.authorId.toString(),
+      questionId: newQuestion.id.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+  })
+})
