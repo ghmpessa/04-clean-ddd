71d6221490bce83ed276761ab514439cbb3b7506
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-23 21:51:38 -0300

    refactor: use InMemoryQuestionsRepository

--- src/domain/forum/application/usecases/create-question.spec.ts
+++ src/domain/forum/application/usecases/create-question.spec.ts
@@ -1,18 +1,22 @@
-import { QuestionsRepository } from '../repositories/questions-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { CreateQuestionUseCase } from './create-question'
 
-const fakeQuestionsRepository: QuestionsRepository = {
-  create: async () => {},
-}
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CreateQuestionUseCase
 
-test('create a question', async () => {
-  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
-
-  const { question } = await createQuestion.execute({
-    authorId: '1',
-    title: 'Title',
-    content: 'New question',
+describe('Create Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
-  expect(question.id).toBeTruthy()
+  it('should be able to create a question', async () => {
+    const { question } = await sut.execute({
+      authorId: '1',
+      title: 'Title',
+      content: 'New question',
+    })
+
+    expect(question.id).toBeTruthy()
+  })
 })
