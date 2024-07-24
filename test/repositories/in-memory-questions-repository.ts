983ec2b594f7c829e536a7ab240283881f955ad6
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-24 17:13:16 -0300

    feat: add findById and delete implementation to InMemoryQuestionsRepository

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -2,9 +2,21 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  async findById(id: string) {
+    const question = this.items.find(
+      (question) => question.id.toString() === id,
+    )
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   public items: Question[] = []
 
-  async findBySlug(slug: string): Promise<Question | null> {
+  async findBySlug(slug: string) {
     const question = this.items.find((question) => question.slug.value === slug)
 
     if (!question) {
@@ -14,7 +26,12 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question
   }
 
-  async create(question: Question): Promise<void> {
+  async create(question: Question) {
     this.items.push(question)
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+    this.items.splice(itemIndex, 1)
   }
 }
