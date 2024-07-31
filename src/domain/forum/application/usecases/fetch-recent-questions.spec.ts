13f85c58bf4307efdc087d5e79ea3a51d8dedeb6
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-30 22:15:31 -0300

    test: ensure usecase returns sorted recent question list

---
+++ src/domain/forum/application/usecases/fetch-recent-questions.spec.ts
@@ -0,0 +1,29 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { FetchRecentQuestionsUseCase } from './fetch-recent-questions'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: FetchRecentQuestionsUseCase
+
+describe('Fetch Recent Questions', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new FetchRecentQuestionsUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to fetch recent questions', async () => {
+    await inMemoryQuestionsRepository.create(makeQuestion({ createdAt: new Date(2024, 0, 20) }))
+    await inMemoryQuestionsRepository.create(makeQuestion({ createdAt: new Date(2024, 0, 14) }))
+    await inMemoryQuestionsRepository.create(makeQuestion({ createdAt: new Date(2024, 0, 16) }))
+    await inMemoryQuestionsRepository.create(makeQuestion({ createdAt: new Date(2024, 0, 18) }))
+
+    const { questions } = await sut.execute({ page: 1 })
+
+    expect(questions).toEqual([
+      expect.objectContaining({ createdAt: new Date(2024, 0, 20) }),
+      expect.objectContaining({ createdAt: new Date(2024, 0, 18) }),
+      expect.objectContaining({ createdAt: new Date(2024, 0, 16) }),
+      expect.objectContaining({ createdAt: new Date(2024, 0, 14) }),
+    ])
+  })
+})
