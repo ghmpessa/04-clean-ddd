8b9ce60e612bba846206cbd842be4aea8200928b
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-30 22:00:25 -0300

    feat: add ChoseBestAnswerUseCase

---
+++ src/domain/forum/application/usecases/chose-question-best-answer.ts
@@ -0,0 +1,50 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+
+interface ChoseQuestionBestAnswerRequest {
+  authorId: string
+  answerId: string
+}
+
+interface ChoseQuestionBestAnswerResponse {
+  question: Question
+}
+
+export class ChoseQuestionBestAnswer {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private answersRepository: AnswersRepository
+  ) {}
+
+  async execute({
+    authorId,
+    answerId
+  }: ChoseQuestionBestAnswerRequest): Promise<ChoseQuestionBestAnswerResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+    
+    const question = await this.questionsRepository.findById(answer.questionId.toString())
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    question.bestAnswerId = answer.id
+
+    await this.questionsRepository.save(question)
+
+    return {
+      question
+    }
+  }
+}
