cb91217307978035842af4cda5bf80326fbf6510
Gustavo Pessa <ghmpessa@gmail.com>
2024-08-01 20:56:38 -0300

    feat: add findManyByAnswerId method implementation to InMemoryAnswerCommentsRepository

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -16,6 +17,17 @@ export class InMemoryAnswerCommentsRepository
     }
 
     return answerComment
+  }
+
+  async findManyByAnswerId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<AnswerComment[]> {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
   }
 
   async create(answerComment: AnswerComment) {
