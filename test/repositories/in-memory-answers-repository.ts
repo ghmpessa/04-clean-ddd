44e3293ede30869fe477b8e13723d563c385b5ed
Gustavo Pessa <ghmpessa@gmail.com>
2024-08-01 12:34:57 -0300

    refactor: lint

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -15,9 +15,9 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer
   }
 
-  async findManyByQuestionId(questionId: string, {  page }: PaginationParams & { questionId: string }) {
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
     const answers = this.items
-      .filter(item => item.questionId.toString() === questionId)
+      .filter((item) => item.questionId.toString() === questionId)
       .slice((page - 1) * 20, page * 20)
 
     return answers
