c61476b3685bd835d65217a081af13deb4c6f512
Gustavo Pessa <ghmpessa@gmail.com>
2024-08-02 21:29:47 -0300

    refactor: add QuestionAttachmentsRepository injection to InMemoryQuestionsRepository

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,8 +1,15 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = []
+
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {}
+
   async findById(id: string) {
     const question = this.items.find(
       (question) => question.id.toString() === id,
@@ -14,8 +21,6 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
     return question
   }
-
-  public items: Question[] = []
 
   async findBySlug(slug: string) {
     const question = this.items.find((question) => question.slug.value === slug)
@@ -47,5 +52,9 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   async delete(question: Question) {
     const itemIndex = this.items.findIndex((item) => item.id === question.id)
     this.items.splice(itemIndex, 1)
+
+    this.questionAttachmentsRepository.deleteManyByQuestionId(
+      question.id.toString(),
+    )
   }
 }
