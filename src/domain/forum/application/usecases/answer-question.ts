911f231ed946c456e6143833189c4aecbb1b5701
Gustavo Pessa <ghmpessa@gmail.com>
2024-08-08 20:56:09 -0300

    refactor: apply AnswerAttachmentList into AnswerQuestion use case

--- src/domain/forum/application/usecases/answer-question.ts
+++ src/domain/forum/application/usecases/answer-question.ts
@@ -2,10 +2,13 @@ import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { Either, right } from '@/core/either'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
   questionId: string
+  attachmentsIds: string[]
   content: string
 }
 
@@ -17,6 +20,7 @@ export class AnswerQuestionUseCase {
   async execute({
     instructorId,
     questionId,
+    attachmentsIds,
     content,
   }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
@@ -24,6 +28,15 @@ export class AnswerQuestionUseCase {
       authorId: new UniqueEntityId(instructorId),
       questionId: new UniqueEntityId(questionId),
     })
+
+    const answerAtachments = attachmentsIds.map((attachmentId) => {
+      return AnswerAttachment.create({
+        attachmentId: new UniqueEntityId(attachmentId),
+        answerId: answer.id,
+      })
+    })
+
+    answer.attachments = new AnswerAttachmentList(answerAtachments)
 
     await this.answersRepository.create(answer)
 
