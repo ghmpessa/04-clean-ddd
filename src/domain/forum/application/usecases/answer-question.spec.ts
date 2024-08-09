b4634e5706fd4c0c36bcce48b3facf189971de0c
Gustavo Pessa <ghmpessa@gmail.com>
2024-08-08 21:00:01 -0300

    test: ensure changes do not break usecase

--- src/domain/forum/application/usecases/answer-question.spec.ts
+++ src/domain/forum/application/usecases/answer-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 let sut: AnswerQuestionUseCase
@@ -15,9 +16,19 @@ describe('Create Answer', () => {
       instructorId: '1',
       questionId: 'Title',
       content: 'New question',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryAnswersRepository.items[0]).toEqual(result.value?.answer)
+    expect(
+      inMemoryAnswersRepository.items[0].attachments.currentItems,
+    ).toHaveLength(2)
+    expect(inMemoryAnswersRepository.items[0].attachments.currentItems).toEqual(
+      [
+        expect.objectContaining({ attachmentId: new UniqueEntityId('1') }),
+        expect.objectContaining({ attachmentId: new UniqueEntityId('2') }),
+      ],
+    )
   })
 })
